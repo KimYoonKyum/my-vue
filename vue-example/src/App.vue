def995126ef03095fd012c0fa64d2d62b119eafb
김윤겸 <newalpha77@gmail.com>
2023-07-19 23:56:31 +0900

    initial commit

---
+++ vue-example/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <app-header
+      v-bind:headerTitle="str"
+      v-on:renew="renewStr"
+  >
+
+  </app-header>
+  <div>
+    {{str}}
+  </div>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+
+export default {
+  name: 'App',
+  components: {
+    'app-header':AppHeader,
+  },
+  data: function() {
+    return {
+      str: 'HEADER'
+    }
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'SENDED HEADER'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-example/src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <h1>{{headerTitle}}</h1>
+    <button v-on:click="onSend">send</button>
+  </header>
+</template>
+
+<script>
+export default {
+  props:['headerTitle'],
+  methods: {
+    onSend: function() {
+      console.log('renew')
+      this.$emit('renew')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
